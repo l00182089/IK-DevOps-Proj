cc9982ffb5e94bc34e6dc9dd572c34a6e7feb1b7
Ibrahim Kako <l00182089@atu.ie>
2025-10-22 00:45:10 +0100

    Add tests to test Character promotion functionality

---
+++ tests/character.test.js
@@ -0,0 +1,11 @@
+const Character = require('../src/character');
+
+test('Tanjiro promotes from Mizunoto to Mizunoe', () => {
+  const tanjiro = new Character("Tanjiro", "Mizunoto");
+  expect(tanjiro.promote()).toBe("Mizunoe");
+});
+
+test('Hashira rank cannot go higher', () => {
+  const giyu = new Character("Giyu", "Hashira");
+  expect(giyu.promote()).toBe("Hashira");
+});
